596f90d7627e0e4f0a899f73ee91709742cb4f5f
lauranenzi <laura.nenzi@gmail.com>
2019-09-27 17:46:22 +0200

    prop city

--- eu.quanticol.moonlight.core/src/test/java/eu/quanticol/moonlight/tests/TestCity.java
+++ eu.quanticol.moonlight.core/src/test/java/eu/quanticol/moonlight/tests/TestCity.java
@@ -5,10 +5,7 @@ package eu.quanticol.moonlight.tests;
 
 import eu.quanticol.moonlight.formula.*;
 import eu.quanticol.moonlight.monitoring.SpatioTemporalMonitoring;
-import eu.quanticol.moonlight.signal.DistanceStructure;
-import eu.quanticol.moonlight.signal.GraphModel;
-import eu.quanticol.moonlight.signal.SpatialModel;
-import eu.quanticol.moonlight.signal.SpatioTemporalSignal;
+import eu.quanticol.moonlight.signal.*;
 import eu.quanticol.moonlight.util.Pair;
 import org.junit.Test;
 
@@ -62,27 +59,67 @@ public class TestCity {
 
     }
 
+
+    @Test
     public void testAtomicPropCity() {
 
         SpatialModel<Double> city = buildingCity();
         double range = 40;
-        DistanceStructure<Double, Double> ds = new DistanceStructure<Double, Double>(x -> x, new DoubleDistance(), 0.0, range, city);
+        DistanceStructure<Double, Double> ds = new DistanceStructure<>(x -> x, new DoubleDistance(), 0.0, range, city);
 
         ArrayList<String> place = new ArrayList<>(Arrays.asList("BusStop", "Hospital", "MetroStop", "MainSquare", "BusStop", "Museum", "MetroStop"));
         ArrayList<Boolean> taxi=new ArrayList<>(Arrays.asList(false,false,true,false,false,true,false));
         ArrayList<Integer> people=new ArrayList<>(Arrays.asList(3,145,67,243,22,103,6));
-        SpatioTemporalSignal<Pair<String, Boolean>> signal = TestUtils.createSpatioTemporalSignal(this.size, 0, 0.1, 10, (t, l) -> new Pair<>(place.get(l),taxi.get(l)));
+        SpatioTemporalSignal<Pair<String, Boolean>> signal = TestUtils.createSpatioTemporalSignal(
+                this.size, 0, 0.1, 10, (t, l) -> new Pair<>(place.get(l),taxi.get(l)));
+
+        assertEquals("Hospital",signal.valuesatT(0).get(1).getFirst());
+        assertEquals(false,signal.valuesatT(0).get(0).getSecond());
 
         HashMap<String, Function<Parameters, Function<Pair<String,Boolean>, Boolean>>> atomic = new HashMap<>();
+        atomic.put("isThereATaxi", p -> (x -> x.getSecond()));
         atomic.put("isThereAStop", p -> (x -> x.getFirst().equals("BusStop") || x.getFirst().equals("MetroStop")));
-        SpatioTemporalMonitoring<Boolean, Pair<String,Boolean>, Boolean> monitor = new SpatioTemporalMonitoring<Boolean, Pair<String, Boolean>, Boolean>(
+        atomic.put("isHopsital", p -> (x -> x.getFirst().equals("Hospital")));
+
+        HashMap<String,Function<SpatialModel<Double>,DistanceStructure<Double, ? extends Object>>> distanceFunctions = new HashMap<>();
+        DistanceStructure<Double, Double> predist = new DistanceStructure<>(x -> x+10, new DoubleDistance(), 0.0, range, city);
+        distanceFunctions.put("dist10", x -> predist);
+
+        Formula somewhereTaxi = new SomewhereFormula("dist10", new AtomicFormula("isThereATaxi"));
+
+        SpatioTemporalMonitoring<Double, Pair<String,Boolean>, Boolean> monitor = new SpatioTemporalMonitoring<>(
                 atomic ,
                 new HashMap<>(),
                 new BooleanDomain(),
                 true);
 
-        BiFunction<Function<Boolean, SpatialModel<Double>>, SpatioTemporalSignal<Pair<String,Boolean>>, SpatioTemporalSignal<Boolean>> m = monitor.monitor(new AtomicFormula("isThereAStop"), null);
+        BiFunction<Function<Double, SpatialModel<Double>>, SpatioTemporalSignal<Pair<String,Boolean>>, SpatioTemporalSignal<Boolean>> m = monitor.monitor(
+                new AtomicFormula("isThereATaxi"), null);
         SpatioTemporalSignal<Boolean> sout = m.apply(t -> city, signal);
+        ArrayList<Signal<Boolean>> signals = sout.getSignals();
+        for( int i=0 ; i<this.size ; i++ ) {
+            assertEquals( taxi.get(i), signals.get(i).valueAt(9));
+        }
+
+
+        BiFunction<Function<Double, SpatialModel<Double>>, SpatioTemporalSignal<Pair<String,Boolean>>, SpatioTemporalSignal<Boolean>> m2 = monitor.monitor(
+                new AtomicFormula("isThereAStop"), null);
+        SpatioTemporalSignal<Boolean> sout2 = m2.apply(t -> city, signal);
+        ArrayList<Signal<Boolean>> signals2 = sout2.getSignals();
+        ArrayList<Boolean> soluz=new ArrayList<>(Arrays.asList(true, false, true, false, true, false, true));
+        for( int i=0 ; i<this.size ; i++ ) {
+            assertEquals( soluz.get(i), signals2.get(i).valueAt(1));
+        }
+
+        BiFunction<Function<Double, SpatialModel<Double>>, SpatioTemporalSignal<Pair<String,Boolean>>, SpatioTemporalSignal<Boolean>> m3 = monitor.monitor(
+                somewhereTaxi, null);
+        SpatioTemporalSignal<Boolean> sout3 = m.apply(t -> city, signal);
+        ArrayList<Signal<Boolean>> signals3 = sout.getSignals();
+        for( int i=0 ; i<this.size ; i++ ) {
+            assertEquals( true, signals.get(i).valueAt(0));
+        }
+
+
 
 
     }
