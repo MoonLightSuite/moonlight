1dd0dfd48508ee21169dd68c267959195d6b54af
Simone <simone.silvetti@gmail.com>
2019-10-14 21:11:06 +0200

    sensor example improvements

--- core/src/main/java/eu/quanticol/moonlight/signal/GraphModelUtility.java
+++ core/src/main/java/eu/quanticol/moonlight/signal/GraphModelUtility.java
@@ -10,8 +10,9 @@ public class GraphModelUtility {
         int size = matrix.length;
         GraphModel<Double> graphModel = new GraphModel<>(size);
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
                 graphModel.add(i, matrix[i][j], j);
+                graphModel.add(j, matrix[j][i], i);
             }
         }
         return graphModel;
--- example/spatio-temporal/sensors/src/main/java/eu/quanticol/moonlight/examples/sensors/Sensors.java
+++ example/spatio-temporal/sensors/src/main/java/eu/quanticol/moonlight/examples/sensors/Sensors.java
@@ -1,14 +1,24 @@
 package eu.quanticol.moonlight.examples.sensors;
 
 import com.mathworks.engine.MatlabEngine;
+import eu.quanticol.moonlight.formula.AtomicFormula;
+import eu.quanticol.moonlight.formula.BooleanDomain;
+import eu.quanticol.moonlight.formula.Parameters;
+import eu.quanticol.moonlight.monitoring.SpatioTemporalMonitoring;
 import eu.quanticol.moonlight.signal.GraphModelUtility;
 import eu.quanticol.moonlight.signal.SpatialModel;
+import eu.quanticol.moonlight.signal.SpatioTemporalSignal;
 import eu.quanticol.moonlight.utility.matlab.MatlabExecutor;
+import eu.quanticol.moonlight.utility.matlab.configurator.MatlabDataConverter;
 
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
 import java.util.function.DoubleFunction;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Sensors {
 
@@ -21,12 +31,30 @@ public class Sensors {
         eng.eval("mobility");
 
         Object[][] trajectory = eng.getVariable("nodes");
-        Object[] nodesType = eng.getVariable("nodes_type");
+        Double[] nodesType = MatlabDataConverter.getArray(eng.getVariable("nodes_type"), Double.class);
         Object[] cgraph1 = eng.getVariable("cgraph1");
         Object[] cgraph2 = eng.getVariable("cgraph2");
         MatlabExecutor.close();
         DoubleFunction<SpatialModel<Double>> tConsumer = t -> GraphModelUtility.fromMatrix((double[][]) cgraph1[(int) Math.floor(t)]);
-        System.out.println();
+        SpatioTemporalSignal<Integer> spatioTemporalSignal = new SpatioTemporalSignal<>(nodesType.length);
+        IntStream.range(0, trajectory.length).forEach(i -> spatioTemporalSignal.add(i, location -> nodesType[location].intValue()));
 
+        HashMap<String, Function<Parameters, Function<Integer, Boolean>>> atomicFormulas = new HashMap<>();
+        atomicFormulas.put("type1", p -> (x -> x == 1));
+        atomicFormulas.put("type2", p -> (x -> x == 2));
+        atomicFormulas.put("type3", p -> (x -> x == 3));
+
+        SpatioTemporalMonitoring<Double, Integer, Boolean> monitor =
+                new SpatioTemporalMonitoring<>(
+                        atomicFormulas,
+                        null,
+                        new BooleanDomain(),
+                        false);
+
+
+        BiFunction<DoubleFunction<SpatialModel<Double>>, SpatioTemporalSignal<Integer>, SpatioTemporalSignal<Boolean>> m =
+                monitor.monitor(new AtomicFormula("type1"), null);
+        SpatioTemporalSignal<Boolean> sout = m.apply(tConsumer, spatioTemporalSignal);
+        System.out.println();
     }
 }
