767ee845e4beb8d7de2aa2276f58c8cbc29daf3f
Michele Loreti <michele.loreti@unicam.it>
2019-10-21 20:10:33 +0200

    New methods in MoonLight script.

--- core/src/main/java/eu/quanticol/moonlight/MoonLightScript.java
+++ core/src/main/java/eu/quanticol/moonlight/MoonLightScript.java
@@ -3,6 +3,10 @@
  */
 package eu.quanticol.moonlight;
 
+import eu.quanticol.moonlight.signal.LocationService;
+import eu.quanticol.moonlight.signal.Signal;
+import eu.quanticol.moonlight.signal.SpatioTemporalSignal;
+
 /**
  * @author loreti
  *
@@ -11,6 +15,10 @@ public interface MoonLightScript {
 	
 	public void monitor( String label, String inputFile , String outputFile );
 	
+	public void monitor( String label, Signal<?> signal , String outputFile );
+
+	public void monitor( String label, LocationService<?> service , SpatioTemporalSignal<?> signal , String outputFile );
+
 	public String[] getMonitors();
 	
 	public String getInfo( String monitor );
