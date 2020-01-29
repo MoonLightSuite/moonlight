12b2a3bfa592ae76fa09c48993e9b133f1d25432
lalica <laura.nenzi@gmail.com>
2020-01-29 01:41:43 +0100

    sensorexamnple

--- distribution/examples/SpatialDynamicEval/plotSpTemp.m
+++ distribution/examples/SpatialDynamicEval/plotSpTemp.m
@@ -1,14 +1,7 @@
-% t =numStepInt;
-% plot(t,timeMoonRepSignal,'b-+',t,timeTalRepSignal,'r--*',t,timeBreachRepSignal,'g-.o',...
-% 'LineWidth',2,'MarkerSize',10);
-% %plot(t,timeMoonRepSignal,'b-+',t,timeBreachRepSignal,'g-.o',...
-% %'LineWidth',2,'MarkerSize',10);
-% set(gca,'FontSize',20)
-% legend('Moonlight','Taliro','Breach','FontSize',20)
-
-
-    Gvor.Nodes.NodeColors = spatialResult(1,:)';
-    p = plot(Gvor,'r','XData',Gvor.Nodes.x,'YData',Gvor.Nodes.y)
-    p.MarkerSize = 8;
-    p.NodeCData = Gvor.Nodes.NodeColors;
-    colorbar
+t =num_nodes_seq;
+plot(t,elapseTimeSeq,'b-+',t,elapseTimeSeq,'r--*',t,elapseTimeSeq,'g-.o',...
+'LineWidth',2,'MarkerSize',10);
+%plot(t,timeMoonRepSignal,'b-+',t,timeBreachRepSignal,'g-.o',...
+%'LineWidth',2,'MarkerSize',10);
+set(gca,'FontSize',20)
+legend('phi1','phi2','phi3','FontSize',20)
