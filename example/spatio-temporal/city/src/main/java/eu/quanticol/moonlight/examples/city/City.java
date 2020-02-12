7f9811189d244406000b97f151f88576c5a9a1d7
loreti <loreti@micheles-mbp.amministrazione.unicam>
2020-02-12 16:56:13 +0100

    [NEW] Example for the smart city scenario.

---
+++ example/spatio-temporal/city/src/main/java/eu/quanticol/moonlight/examples/city/City.java
@@ -0,0 +1,155 @@
+package eu.quanticol.moonlight.examples.city;
+
+import eu.quanticol.moonlight.formula.*;
+import eu.quanticol.moonlight.monitoring.SpatioTemporalMonitoring;
+import eu.quanticol.moonlight.monitoring.spatiotemporal.SpatioTemporalMonitor;
+import eu.quanticol.moonlight.signal.*;
+import eu.quanticol.moonlight.util.Pair;
+import eu.quanticol.moonlight.util.TestUtils;
+import eu.quanticol.moonlight.util.Triple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.DoubleFunction;
+import java.util.function.Function;
+
+
+public class City {
+
+    private static SpatialModel<Double> city = buildingCity();
+    private static final double range = 40;
+    private static final int SIZE = 7;
+    private static final DoubleDomain doubleDomain = new DoubleDomain();
+    private static final BooleanDomain booleanDomain = new BooleanDomain();
+
+    static void setUp() { //questo metodo viene eseguito una volta quando viene caricata la classe, visto che la città è la stessa per tutti i test è il posto giusto dove definirla
+        city = buildingCity();
+    }
+
+    public static void main(String[] argv) {
+
+        //// SpatioTemporalSignal
+        List<String> places = Arrays.asList("BusStop", "Hospital", "MetroStop", "MainSquare", "BusStop", "Museum", "MetroStop");
+        List<Boolean> taxiAvailability = Arrays.asList(false, false, true, false, false, true, false);
+        List<Integer> peopleAtPlaces = Arrays.asList(3, 145, 67, 243, 22, 103, 6);
+        SpatioTemporalSignal<Triple<String, Boolean, Integer>> signal = createSpatioTemporalSignal(SIZE, 0, 1, 20.0,
+                (t, l) -> new Triple<>(places.get(l), taxiAvailability.get(l), peopleAtPlaces.get(l)));
+
+
+        //// Loc Service Static ///
+        LocationService<Double> locService = TestUtils.createLocServiceStatic(0, 1, 20.0,city);
+
+
+        ////  P1 = "isThereATaxi" ////
+        SpatioTemporalMonitor<Double,Triple<String, Boolean, Integer>,Boolean> m = isThereATaxi();
+
+        SpatioTemporalSignal<Boolean> sout = m.monitor(locService, signal);
+        List<Signal<Boolean>> signals = sout.getSignals();
+
+        System.out.println(signals.get(0).valueAt(0));
+
+
+        ////  P2 = evTaxi ////
+        SpatioTemporalMonitor<Double,Triple<String, Boolean, Integer>,Boolean> m7 = eventuallyATaxi(0,20);
+        SpatioTemporalSignal<Boolean> sout7 = m7.monitor(locService, signal);
+        List<Signal<Boolean>> signals7 = sout7.getSignals();
+        System.out.println(signals7.get(0).valueAt(0));
+//
+//        ////  6 ////
+        SpatioTemporalMonitor<Double,Triple<String, Boolean, Integer>,Boolean> m6 = ifTaxiReachStop(0,10);
+        SpatioTemporalSignal<Boolean> sout6 = m6.monitor(locService, signal);
+        List<Signal<Boolean>> signals6 = sout6.getSignals();
+        System.out.println(signals6.get(0).valueAt(0));
+
+
+    }
+
+
+    private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean> ifTaxiReachStop(double from, double to) {
+//      Formula iftaxiReachStop = new OrFormula(new NegationFormula(new AtomicFormula("isThereATaxi")), taxiReachStop);
+		return SpatioTemporalMonitor.orMonitor(
+				SpatioTemporalMonitor.negationMonitor(isThereATaxi(), booleanDomain), booleanDomain, 
+				taxiReachStop(from,to)
+				);
+	}
+
+	private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean>  taxiReachStop(double from, double to) {
+//      Formula taxiReachStop = new ReachFormula(
+//              new AtomicFormula("isThereATaxi"),"no", "dist3", stopReacMainsquare );
+		return SpatioTemporalMonitor.reachMonitor(isThereATaxi(), distance(from,to), stopReachMainSquare(), booleanDomain);
+	}
+
+	private static Function<SpatialModel<Double>, DistanceStructure<Double, ?>>  distance(double from, double to) {
+		return g -> new DistanceStructure<>(x -> x, new DoubleDistance(), from, to, g);
+	}
+
+	private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean>   stopReachMainSquare() {
+//      Formula somewhereTaxi = new SomewhereFormula("distX", new AtomicFormula("isThereATaxi"));
+//      Formula stopReacMainsquare = new ReachFormula(
+//              new AtomicFormula("isThereAStop"),"no", "dist10", new AtomicFormula("isMainSquare") );
+		return SpatioTemporalMonitor.reachMonitor(isThereAStop(),null,isMainSquare(),booleanDomain);
+	}
+
+	private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean> eventuallyATaxi(double a,
+			double b) {
+//      Formula evTaxi = new EventuallyFormula( new AtomicFormula("isThereATaxi"), new Interval(0,20));
+    	return SpatioTemporalMonitor.eventuallyMonitor(isThereATaxi(), new Interval(a,b),booleanDomain);
+	}
+
+	private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean> isThereATaxi() {
+		return SpatioTemporalMonitor.atomicMonitor(p -> p.getSecond());
+	}
+
+	private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean> isThereAStop() {
+		return SpatioTemporalMonitor.atomicMonitor((x -> "BusStop".equals(x.getFirst()) || "MetroStop".equals(x.getFirst())));
+	}
+
+	private static SpatioTemporalMonitor<Double, Triple<String, Boolean, Integer>, Boolean> isMainSquare() {
+		return SpatioTemporalMonitor.atomicMonitor((x -> "MainSquare".equals(x.getFirst())));
+	}
+
+	private static SpatialModel<Double> buildingCity() { //metto alla fine tutti i metodi privati di servizio.
+        HashMap<Pair<Integer, Integer>, Double> cityMap = new HashMap<>();
+        cityMap.put(new Pair<>(0, 1), 2.0);
+        cityMap.put(new Pair<>(1, 0), 2.0);
+        cityMap.put(new Pair<>(0, 5), 2.0);
+        cityMap.put(new Pair<>(5, 0), 2.0);
+        cityMap.put(new Pair<>(1, 2), 9.0);
+        cityMap.put(new Pair<>(2, 1), 9.0);
+        cityMap.put(new Pair<>(2, 3), 3.0);
+        cityMap.put(new Pair<>(3, 2), 3.0);
+        cityMap.put(new Pair<>(3, 4), 6.0);
+        cityMap.put(new Pair<>(4, 3), 6.0);
+        cityMap.put(new Pair<>(4, 5), 7.0);
+        cityMap.put(new Pair<>(5, 4), 7.0);
+        cityMap.put(new Pair<>(6, 1), 4.0);
+        cityMap.put(new Pair<>(1, 6), 4.0);
+        cityMap.put(new Pair<>(6, 3), 15.0);
+        cityMap.put(new Pair<>(3, 6), 15.0);
+        return TestUtils.createSpatialModel(SIZE, cityMap);
+    }
+
+    private static <T> SpatioTemporalSignal<T> createSpatioTemporalSignal(int size, double start, double dt, double end, BiFunction<Double, Integer, T> f) {
+        SpatioTemporalSignal<T> s = new SpatioTemporalSignal(size);
+
+        for(double time = start; time < end; time += dt) {
+            double finalTime = time;
+            s.add(time, (i) -> {
+                return f.apply(finalTime, i);
+            });
+        }
+
+        s.add(end, (i) -> {
+            return f.apply(end, i);
+        });
+        return s;
+    }
+
+
+}
+
+
+
