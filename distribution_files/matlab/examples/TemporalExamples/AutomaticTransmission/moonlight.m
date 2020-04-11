02f64f22284a5408c3864fc67ab862f6eced66ec
Ezio Bartocci <ezio.bartocci@gmail.com>
2020-04-11 17:53:13 +0200

    Ezio: polishing of the scripts

--- distribution_files/matlab/examples/TemporalExamples/AutomaticTransmission/moonlight.m
+++ distribution_files/matlab/examples/TemporalExamples/AutomaticTransmission/moonlight.m
@@ -1,12 +1,7 @@
-clear;
+%% Initializing the script
 
-
-%Generate a monitor object from the script fine multiple_spec.mls 
-%this object is an implementation of MoonlightEngine class, please 
-%refer to the doc of this class for more details (ex. write in console 
-%"doc MoonlightEngine" ) please, open multiple_spec.mls 
-monitor = MoonlightEngine.load("multiple_spec");
-
+clear;       %clear all the memory
+close all;   %close all the open windows
 
 %% Configuration file for simulating the Simulink model "autotrans_mod04"
 % Variables:
@@ -15,13 +10,22 @@ monitor = MoonlightEngine.load("multiple_spec");
 %    - piecewise_throttle - define the input signal of the throttle
 %    - piecewise_break - define the input signal of the break
 
-dt     =  0.02;
-stime  =  80;
-solver = 'ode5';
+dt            =  0.02;
+stime         =  80;
+solver        = 'ode5';
+
+model         = 'autotrans_mod04'
+input_labels  = {'time', 'throttle', 'brake'};
+output_labels = {'engine speed in rpm', 'vehicle speed in mph', 'gear'};
+
+fprintf('Settings\n\n');
+fprintf('\t dt     = %f \n',dt    );
+fprintf('\t stime  = %f \n',stime );
+fprintf('\t solver = %s \n\n',solver);
+
 
 picewise_throttle = [  0,  5,   5, 10, 10, 15, 15, 20, 20, 25, 25, stime;   %time
-                      52, 52,  95, 95, 60, 60, 85, 85, 75, 75, 80,   80     %value
-                       ];  %time
+                      52, 52,  95, 95, 60, 60, 85, 85, 75, 75, 80,   80];   %value
                    
 picewise_brake    = [  0, stime;   %time
                        0,    0];   %value
@@ -29,6 +33,8 @@ picewise_brake    = [  0, stime;   %time
                    
 %% Generating input signals
 %
+
+fprintf('Generating input signals\n');
 
 time = 0:dt:stime;
 
@@ -48,26 +54,52 @@ input(:,2) = input_throttle';
 input(:,3) = input_brake';
 
 %% Simulating Simulink Model 
-[time, output] = simSimulinkModel ('autotrans_mod04', input, solver, dt);
 
+fprintf('Simulation of Simulink Model \n');
+
+[time, output] = simSimulinkModel (model, input, solver, dt);
+
+fprintf('Plotting \n');
 
 %% Plotting the simulation
-plotting (input, output);
+plotting (input, output, input_labels, output_labels);
+
+
+%% Monitoring initialization
+
+fprintf('Monitoring initialization\n');
+
+%Generate a monitor object from the script fine multiple_spec.mls 
+%this object is an implementation of MoonlightEngine class, please 
+%refer to the doc of this class for more details (ex. write in console 
+%"doc MoonlightEngine" ) please, open multiple_spec.mls 
+monitor = MoonlightEngine.load("multiple_spec");
 
 
 %% Monitoring
 
-bMonitorResult1  = monitor.temporalMonitor("BooleanMonitorSpec1",     time,output,4000); 
-qMonitorResult1  = monitor.temporalMonitor("QuantitativeMonitorSpec1",time,output,4000); 
+fprintf('Monitoring\n');
+tic
+bMonitorResult1  = monitor.temporalMonitor("BooleanMonitorSpec1",     time, output, 4000); 
+toc 
+tic
+qMonitorResult1  = monitor.temporalMonitor("QuantitativeMonitorSpec1",time, output, 4000); 
+toc
+tic
+bMonitorResult2  = monitor.temporalMonitor("BooleanMonitorSpec2",     time, output,[3000, 120]); 
+toc
+tic
+qMonitorResult2  = monitor.temporalMonitor("QuantitativeMonitorSpec2",time, output,[3000, 120]); 
+toc
+tic
+bMonitorResult6  = monitor.temporalMonitor("BooleanMonitorSpec6",     time, output,[3000, 120, 10]);
+toc
+tic
+qMonitorResult6  = monitor.temporalMonitor("QuantitativeMonitorSpec6",time, output,[3000, 120, 10]); 
+toc
 
-bMonitorResult2  = monitor.temporalMonitor("BooleanMonitorSpec2",     time,output,[3000, 120]); 
-qMonitorResult2  = monitor.temporalMonitor("QuantitativeMonitorSpec2",time,output,[3000, 120]); 
-
-bMonitorResult6  = monitor.temporalMonitor("BooleanMonitorSpec6",     time,output,[3000, 120, 10]); 
-qMonitorResult6  = monitor.temporalMonitor("QuantitativeMonitorSpec6",time,output,[3000, 120, 10]); 
-
-bMonitorResult7  = monitor.temporalMonitor("BooleanMonitorSpec7",     time,output,[3000, 120, 10]); 
-qMonitorResult7  = monitor.temporalMonitor("QuantitativeMonitorSpec7",time,output,[3000, 120, 10]); 
+bMonitorResult7  = monitor.temporalMonitor("BooleanMonitorSpec7",     time, output,[3000, 120, 10]); 
+qMonitorResult7  = monitor.temporalMonitor("QuantitativeMonitorSpec7",time, output,[3000, 120, 10]); 
 
 
 
