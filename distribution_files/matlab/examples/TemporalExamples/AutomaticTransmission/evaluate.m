917a4f38e9b6baae7eecf171c3b875ca8a8b40bb
Ezio Bartocci <ezio.bartocci@gmail.com>
2020-06-06 22:44:59 +0200

    Ezio: update evaluation

--- distribution_files/matlab/examples/TemporalExamples/AutomaticTransmission/evaluate.m
+++ distribution_files/matlab/examples/TemporalExamples/AutomaticTransmission/evaluate.m
@@ -1,7 +1,7 @@
 clear;       %clear all the memory
 close all;   %close all the open windows
 
-NTest= 100;
+NTest= 2;
 NExp = 50;
 STime = 32;
 Dt    = 0.01;
@@ -74,6 +74,52 @@ staliro_rob_time_spec4 = staliro_rob_time_spec4 (:);
 
 currDate = strrep(datestr(datetime), ' ', '_');
 
+save (strcat('./test/meta/',currDate,'/meta.mat'), 'NTest', 'NExp', 'STime', 'Dt', 'testdirs', 'moonlight_rob_time_spec1', 'moonlight_rob_time_spec2', 'moonlight_rob_time_spec3', 'moonlight_rob_time_spec4', 'breach_rob_time_spec1', 'breach_rob_time_spec2' , 'breach_rob_time_spec3' , 'breach_rob_time_spec4' , 'staliro_rob_time_spec1','staliro_rob_time_spec2','staliro_rob_time_spec3','staliro_rob_time_spec4');
+
+hold on 
+legendEntries = {'Moonlight Spec1' 'Moonlight Spec2' 'Moonlight Spec3' 'Moonlight Spec4' 'Breach Spec1' 'Breach Spec2' 'Breach Spec3' 'Breach Spec4' 'STaliro Spec1' 'STaliro Spec2' 'STaliro Spec3' 'STaliro Spec4'};
+
+boxplot(moonlight_rob_time_spec1, 'Color', [0.4 0.4 0.4],  'position', -0.7, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line', 'labels', 'Spec 1')
+plot(NaN,1,'color',[0.4 0.4 0.4]); %// dummy plot for legend
+
+boxplot(moonlight_rob_time_spec2, 'Color', [0.4 0.5 0.5], 'position', -0.6, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line', 'labels', 'Spec 1')
+plot(NaN,1,'color',[0.4 0.5 0.5]); %// dummy plot for legend
+boxplot(moonlight_rob_time_spec3, 'Color', [0.4 0.6 0.6], 'position', -0.5, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.4 0.6 0.6]); %// dummy plot for legend
+boxplot(moonlight_rob_time_spec4, 'Color', [0.4 0.7 0.7], 'position', -0.4, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.4 0.7 0.7]); %// dummy plot for legend
+
+boxplot(breach_rob_time_spec1, 'Color', [0.6 0.4 0.4], 'position', -0.3, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.6 0.4 0.4]); %// dummy plot for legend
+
+boxplot(breach_rob_time_spec2, 'Color', [0.6 0.5 0.5], 'position', -0.2, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.6 0.5 0.5]); %// dummy plot for legend
+
+boxplot(breach_rob_time_spec3, 'Color', [0.6 0.6 0.6], 'position', -0.1, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.6 0.6 0.6]); %// dummy plot for legend
+
+boxplot(breach_rob_time_spec4, 'Color', [0.6 0.7 0.7], 'position', 0.0, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.6 0.7 0.7]); %// dummy plot for legend
+
+boxplot(staliro_rob_time_spec1, 'Color', [0.8 0.4 0.4], 'position', 0.1, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.8 0.4 0.4]); %// dummy plot for legend
+boxplot(staliro_rob_time_spec2, 'Color', [0.8 0.5 0.5], 'position', 0.2, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.8 0.5 0.5]); %// dummy plot for legend
+
+boxplot(staliro_rob_time_spec3, 'Color', [0.8 0.6 0.6], 'position', 0.3, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.8 0.6 0.6]); %// dummy plot for legend
+boxplot(staliro_rob_time_spec4, 'Color', [0.8 0.7 0.7], 'position', 0.4, 'boxstyle','filled', 'widths',0.05, 'MedianStyle', 'line')
+plot(NaN,1,'color',[0.8 0.7 0.7]); %// dummy plot for legend
+
+
+ylim([0 0.12]) 
+xlim([-0.9 0.5])
+
+legend(legendEntries);
+ylabel('Execution time (sec.)'); grid on;
+
+status = mkdir('test', 'meta');
+status = mkdir('test/meta/',currDate);
 
 save (strcat('./test/meta/',currDate,'/meta.mat'), 'NTest', 'NExp', 'STime', 'Dt', 'testdirs', 'moonlight_rob_time_spec1', 'moonlight_rob_time_spec2', 'moonlight_rob_time_spec3', 'moonlight_rob_time_spec4', 'breach_rob_time_spec1', 'breach_rob_time_spec2' , 'breach_rob_time_spec3' , 'breach_rob_time_spec4' , 'staliro_rob_time_spec1','staliro_rob_time_spec2','staliro_rob_time_spec3','staliro_rob_time_spec4');
 
