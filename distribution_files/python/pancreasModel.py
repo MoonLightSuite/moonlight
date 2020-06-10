b74aa7c245429a8748093cf52ff81d8b3be9fbff
lalica <laura.nenzi@gmail.com>
2020-06-10 18:36:09 +0200

    pancreas e newMOnAuto

--- distribution_files/python/pancreasModel.py
+++ distribution_files/python/pancreasModel.py
@@ -59,25 +59,3 @@ def simulation(timeOfMeals, dGs, model):
         ttot = np.hstack([ttot, time + ttot[-1]])
     return ttot, ytot
 
-
-Kd = [0, -0.0602, -0.0573, -0.06002, -0.0624]
-Ki = [0, -3.53e-07, -3e-07, -1.17e-07, -7.55e-07]
-Kp = [0, -6.17e-04, -6.39e-04, -6.76e-04, -5.42e-04]
-
-pidC1 = lambda x, t: modelPID(x, t, Kp[1], Ki[1], Kd[1])
-pidC2 = lambda x, t: modelPID(x, t, Kp[2], Ki[2], Kd[2])
-pidC3 = lambda x, t: modelPID(x, t, Kp[3], Ki[3], Kd[3])
-pidC4 = lambda x, t: modelPID(x, t, Kp[4], Ki[4], Kd[4])
-
-pidC1Noise = lambda x, t: modelPID(x, t, Kp[1], Ki[1], Kd[1])
-
-
-t_meal1 = np.random.normal(300, 60)
-t_meal2 = np.random.normal(300, 60)
-t_meal3 = 1440 - t_meal1 - t_meal2
-dg1 = np.random.normal(40, 10)
-dg2 = np.random.normal(90, 10)
-dg3 = np.random.normal(60, 10)
-
-t, y = simulation([t_meal1, t_meal2, t_meal3], [dg1, dg2, dg3], pidC3)
-y = y[:,0];
