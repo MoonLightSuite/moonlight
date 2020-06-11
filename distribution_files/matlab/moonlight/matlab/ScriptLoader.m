73e3137f9c8b841d25ff4c538c83e3fc6a7c511a
Simone <simone.silvetti@gmail.com>
2020-06-11 10:26:34 +0200

    [matlab] fix bugs- part1

--- distribution_files/matlab/moonlight/matlab/ScriptLoader.m
+++ distribution_files/matlab/moonlight/matlab/ScriptLoader.m
@@ -7,14 +7,15 @@ classdef ScriptLoader
             warning('on','all');
         end
         function moonlightScript = loadFromText(script)
-            monlightScriptPath=fullfile(tempdir,'moonlight_script.mls');
+            fileName = strcat("moonlight",extractBefore(char(java.util.UUID.randomUUID),"-"));
+            monlightScriptPath=fullfile(tempdir,"moonlight",fileName+".mls");
             monlightScriptFile = fopen(monlightScriptPath,'w');
             for line = script
                 fprintf(monlightScriptFile, strcat(line,"\n"));
             end
-            ScriptLoader.loadInnerFromText(monlightScriptPath);
+            ScriptLoader.loadInnerFromText(fileName,monlightScriptPath);
             warning('off','all');
-            moonlightScript=MoonlightScript(eval("moonlight.script.Script"+"moonlight_script"));
+            moonlightScript=MoonlightScript(eval("moonlight.script.Script"+fileName));
             warning('on','all');
         end
         function loadInnerFromFile(filename)
@@ -31,13 +32,12 @@ classdef ScriptLoader
             javaaddpath(fullfile(getenv("MOONLIGHT_FOLDER"),"moonlight", "script",filename+".jar"));
             warning('on','all');
         end
-         function loadInnerFromText(filename)
+         function loadInnerFromText(filename,path)
             % class static constructor
-            [status, out] = system("java -jar "+fullfile(getenv("MOONLIGHT_FOLDER"),"moonlight","jar","moonlight.jar "+filename+" "+tempdir));
+            [status, out] = system("java -jar "+fullfile(getenv("MOONLIGHT_FOLDER"),"moonlight","jar","moonlight.jar "+path+" "+tempdir));
             if(status~=0)
                 throw(MException("","PARSER OF THE SCRIPT FAILED "+out))
             end
-            filename="moonlight_script";
             [status, out] = system("jar -cvf "+fullfile(getenv("MOONLIGHT_FOLDER"),"moonlight", "script",filename+".jar")+" -C "+tempdir+" "+fullfile("moonlight","script","Script"+filename+".class"));
             if(status~=0)
                 throw(MException("","CREATION OF THE JAR FAILED \n"+out))
