c8e0d63089446c70aa178653af32a67932aeb565
loreti <michele.loreti@unicam.it>
2020-06-15 19:23:12 +0200

    Removed unused field from ReachFormula.

--- core/src/main/java/eu/quanticol/moonlight/formula/ReachFormula.java
+++ core/src/main/java/eu/quanticol/moonlight/formula/ReachFormula.java
@@ -9,21 +9,18 @@ package eu.quanticol.moonlight.formula;
  */
 public class ReachFormula implements Formula {
 	
-	private final String intervalId;
 	private final String distanceFunctionId;
 	private final Formula firstArgument;
 	private final Formula secondArgument;
 	
 	
 	/**
-	 * @param intervalId
 	 * @param distanceFunctionId
 	 * @param firstArgument
 	 * @param secondArgument
 	 */
-	public ReachFormula(Formula firstArgument, String intervalId, String distanceFunctionId, Formula secondArgument) {
+	public ReachFormula(Formula firstArgument, String distanceFunctionId, Formula secondArgument) {
 		super();
-		this.intervalId = intervalId;
 		this.distanceFunctionId = distanceFunctionId;
 		this.firstArgument = firstArgument;
 		this.secondArgument = secondArgument;
@@ -33,14 +30,6 @@ public class ReachFormula implements Formula {
 	@Override
 	public <T, R> R accept(FormulaVisitor<T, R> visitor, T parameters) {
 		return visitor.visit(this, parameters);
-	}
-
-
-	/**
-	 * @return the intervalId
-	 */
-	public String getIntervalId() {
-		return intervalId;
 	}
 
 
