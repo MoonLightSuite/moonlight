7bb34b623e8cf57d6b9f014d3c4280e626e9ec4c
lalica <laura.nenzi@gmail.com>
2020-06-18 13:31:54 +0200

    testServer

--- distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/SpatialEvalPaper/testSensorLocal.m
+++ distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/SpatialEvalPaper/testSensorLocal.m
@@ -3,7 +3,7 @@ clear;       %clear all the memory
 close all;   %close all the open windows
 
 elapseTimeSeq = [];
-num_exp = 1;
+num_exp = 100;
 num_nodes_seq = 10: 50: 1000;
 
 
@@ -18,7 +18,7 @@ moonlight_rob_time_spec3 = [];
 moonlight_rob_time_spec4 = [];
 numSteps =1;
 currDate = strrep(datestr(datetime), ' ', '_');
-status = mkdir('test',currDate);
+status = mkdir('testLocal',currDate);
 for num_nodes = num_nodes_seq
           
     %% Generating input signals
@@ -36,7 +36,7 @@ for num_nodes = num_nodes_seq
     moonlight_rob_time_spec3 = [moonlight_rob_time_spec3, rob_time_results(3)];
     moonlight_rob_time_spec4 = [moonlight_rob_time_spec4, rob_time_results(4)];
     
-    fprintf('Specification (Satisfaction)  - Moonlight Times (sec): p1=%f, p2=%f, p3=%f, p4=%f', b_time_results(1), b_time_results(2), b_time_results(3), b_time_results(4));
+    fprintf('Specification (Satisfaction)  - Moonlight Times (sec): p1=%f, p2=%f, p3=%f, p4=%f  \n', b_time_results(1), b_time_results(2), b_time_results(3), b_time_results(4));
     fprintf('Specification (Robustness)  - Moonlight Times (sec): p1=%f, p2=%f, p3=%f, p4=%f', rob_time_results(1), rob_time_results(2), rob_time_results(3), rob_time_results(4));
 
 end
@@ -55,23 +55,23 @@ end
 % save (strcat('./test/',currDate,'/monitoring_moonlight.mat'), 'moonlight_robust_spec1', 'moonlight_robust_spec2', 'moonlight_robust_spec3', 'moonlight_robust_spec4','moonlight_robust_time_spec1', 'moonlight_robust_time_spec2', 'moonlight_robust_time_spec3', 'moonlight_robust_time_spec4');
  %save (strcat('./test/',currDate,'/simulation.mat'), 'time', 'spatialModel,', 'num_exp', 'signalInput');
 
-save (strcat('./test/',currDate,'/dataInput.mat'), 'time', 'spatialModel','signalInput','num_nodes_seq', 'num_exp', 'numSteps');
-save (strcat('./test/',currDate,'/moonlight_sat_times_stat.mat'), 'moonlight_sat_time_spec1', 'moonlight_sat_time_spec2', 'moonlight_sat_time_spec3', 'moonlight_sat_time_spec4');
-save (strcat('./test/',currDate,'/moonlight_rob_times_stat.mat'), 'moonlight_rob_time_spec1', 'moonlight_rob_time_spec2', 'moonlight_rob_time_spec3', 'moonlight_rob_time_spec4');
+save (strcat('./testLocal/',currDate,'/dataInput.mat'), 'time', 'spatialModel','signalInput','num_nodes_seq', 'num_exp', 'numSteps');
+save (strcat('./testLocal/',currDate,'/moonlight_sat_times_stat.mat'), 'moonlight_sat_time_spec1', 'moonlight_sat_time_spec2', 'moonlight_sat_time_spec3', 'moonlight_sat_time_spec4');
+save (strcat('./testLocal/',currDate,'/moonlight_rob_times_stat.mat'), 'moonlight_rob_time_spec1', 'moonlight_rob_time_spec2', 'moonlight_rob_time_spec3', 'moonlight_rob_time_spec4');
 %save (strcat('./test/',currDate,'/monitoring_moonlight.mat'), 'moonlight_robust_spec1', 'moonlight_robust_spec2', 'moonlight_robust_spec3', 'moonlight_robust_spec4','moonlight_robust_time_spec1', 'moonlight_robust_time_spec2', 'moonlight_robust_time_spec3', 'moonlight_robust_time_spec4');
 
 
-% figure; % open a new figure window
-% plot(num_nodes_seq,moonlight_sat_time_spec1,'b','LineWidth',2);
-% hold on;
-% plot(num_nodes_seq,moonlight_rob_time_spec1,'b--','LineWidth',2);
-% plot(num_nodes_seq,moonlight_sat_time_spec2,'r','LineWidth',2);
-% plot(num_nodes_seq,moonlight_rob_time_spec2,'r--','LineWidth',2);
-% xlabel('number of nodes');
-% ylabel('time');
-% legend('P1','P1q','P2','P2q')
-% set(gca,'FontSize',18); 
-% 
+figure; % open a new figure window
+plot(num_nodes_seq,moonlight_sat_time_spec1,'b','LineWidth',2);
+hold on;
+plot(num_nodes_seq,moonlight_rob_time_spec1,'b--','LineWidth',2);
+plot(num_nodes_seq,moonlight_sat_time_spec2,'r','LineWidth',2);
+plot(num_nodes_seq,moonlight_rob_time_spec2,'r--','LineWidth',2);
+xlabel('number of nodes');
+ylabel('time');
+legend('P1','P1q','P2','P2q')
+set(gca,'FontSize',18); 
+
 % 
 % figure; % open a new figure window
 % plot(num_nodes_seq,moonlight_sat_time_spec3,'g','LineWidth',2);
