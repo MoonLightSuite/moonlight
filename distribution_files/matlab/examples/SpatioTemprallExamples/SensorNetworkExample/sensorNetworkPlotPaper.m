7a20dede2da828d8ff323cd4ae844b588a5db4d8
lalica <laura.nenzi@gmail.com>
2020-06-19 00:09:58 +0200

    exoTemp

--- distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/sensorNetworkPlotPaper.m
+++ distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/sensorNetworkPlotPaper.m
@@ -1,21 +1,23 @@
-% clear;
-% close all;
+clear;
+close all;
 
-% %generation of the data
-% numSteps        = 1;
-% num_nodes       = 5;
-% framePlot = false; % to enable or disable the plot of the graph
-% [spatialModel,time,signal]= sensorModel(num_nodes,numSteps, framePlot);
-load('dataInput.mat');
+%generation of the data
+numSteps        = 1;
+num_nodes       = 20;
+framePlot = false; % to enable or disable the plot of the graph
+[spatialModelv,spatialModelc,time,values]= sensorModel(num_nodes,numSteps, framePlot);
+%load('dataInput.mat');
 numframe = 1;
-plotGraph(spatialModel, numframe , 'node');
+plotGraph(spatialModelc, numframe , 'node');
 
-%%%%%% monitor  %%%%
+%% property  
 moonlightScript = ScriptLoader.loadFromFile("test");
 moonlightScript.setBooleanDomain();
-% MyFirstFormula = ( nodeType==3 ) reach (hop)[0, 1] ( nodeType==1 );
-boolSpTempMonitor = moonlightScript.getMonitor("ReachFormula");
-%%%%% phi 1 %%%%%%
-result = boolSpTempMonitor.monitor(spatialModel,time,values);
+% P1 = ( nodeType==2 ) reach (hop)[0, 5] ( nodeType==1 );
+boolSpTempMonitor = moonlightScript.getMonitor("P1");
 
-plotResults(spatialModel, result, true)
+%% evaluation
+inputModel = spatialModelc;
+result = boolSpTempMonitor.monitor(inputModel,time,values);
+
+plotResults(inputModel, result, true)
--- distribution_files/matlab/examples/TemporalExamples/AutomaticTransmission/evaluate.m
+++ distribution_files/matlab/examples/TemporalExamples/AutomaticTransmission/evaluate.m
@@ -1,8 +1,8 @@
 clear;       %clear all the memory
 close all;   %close all the open windows
 
-NTest = 1;
-NExp  = 10;
+NTest = 100;
+NExp  = 50;
 STime = 64;
 Dt    = 0.01;
 testdirs = {};
