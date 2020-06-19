90bb4dd2ecbb88e60e506ad418c6818386c509c8
Simone <simone.silvetti@gmail.com>
2020-06-19 16:49:38 +0200

    [matlab] final version

--- distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/sensorNetworkTestScript.m
+++ distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/sensorNetworkTestScript.m
@@ -2,7 +2,7 @@ clear;
 close all;
 
 %% STEP 1: define a spatial-temporal signal.
-numSteps        = 10;   % number of frames
+numSteps        = 7;   % number of frames
 num_nodes       = 10;    % number of nodes
 framePlot = false; % to enable or disable the plot of the graph
 % see the sensorModel function for the description of the output
@@ -11,8 +11,10 @@ framePlot = false; % to enable or disable the plot of the graph
 script= [
 "signal { int nodeType; real battery; real temperature; }",...
 "space {edges { int hop; real dist; }}",...
-"domain boolean;",... 
-"formula P1 = ( nodeType==3 ) reach (hop)[0, 5] ( nodeType==1 ) ;"
+"domain minmax;",... 
+"formula P1 = ( nodeType==3 ) reach (hop)[0, 1] ( nodeType==1 | nodeType==2 ) ;",...
+"formula P2 = escape(hop)[5,inf] (battery > 0.5) ;",...
+"formula P3 = somewhere(dist)[0,300] (battery > 0.5))"
 ];
 
 %% STEP 2: loading the Moonlight Script for a stringArray variable (i.e., script)
@@ -22,7 +24,7 @@ moonlightScript = ScriptLoader.loadFromText(script);
 % moonlightScript.setMinMaxDomain();
 
 %% STEP 4: getting the monitor associated with a target formula
-boolSpTempMonitor = moonlightScript.getMonitor("MyFirstFormula");
+spTempMonitor = moonlightScript.getMonitor("P3");
 
 %% STEP 5: monitor the signal 
-result = boolSpTempMonitor.monitor(spatialModel,time,values);
+result = spTempMonitor.monitor(spatialModel,time,values);
