1b4c270332e74aa67c9475f06a4813664b14124b
lalica <laura.nenzi@gmail.com>
2020-06-21 17:44:43 +0200

    esp

--- distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/SpatialEvalPaper/plotResult.m
+++ distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/SpatialEvalPaper/plotResult.m
@@ -1,17 +1,19 @@
-function plotResult(num_nodes_seq,node_results)
 figure; % open a new figure window
-plot(num_nodes_seq,r1,'LineWidth',2);
+plot(N,moonlight_sat_time_spec1,'r','LineWidth',2);
 hold on;
-plot(num_nodes_seq,r2,'LineWidth',2);
-plot(num_nodes_seq,r3,'LineWidth',2);
-plot(num_nodes_seq,r4,'LineWidth',2);
-xlabel('number of nodes');
+plot(N,moonlight_rob_time_spec1,'r--','LineWidth',2);
+plot(N,moonlight_sat_time_spec2,'b','LineWidth',2);
+plot(N,moonlight_rob_time_spec2,'b--','LineWidth',2);
+plot(N,moonlight_sat_time_spec3,'g','LineWidth',2);
+plot(N,moonlight_rob_time_spec3,'g--','LineWidth',2);
+% plot(num_nodes_seq,moonlight_sat_time_spec4,'c','LineWidth',2);
+% plot(num_nodes_seq,moonlight_rob_time_spec4,'c--','LineWidth',2);
+% plot(num_nodes_seq,moonlight_sat_time_spec5,'m','LineWidth',2);
+% plot(num_nodes_seq,moonlight_rob_time_spec5,'m--','LineWidth',2);
+ xlabel('number of nodes');
 ylabel('time');
-legend('P1','P2','P3','P4')
+legend('P1','P1q','P2','P2q','P3','P3q')
+%legend('P1','P1q','P2','P2q','P3','P3q','P4','P4q','P5','P5q')
 set(gca,'FontSize',18); 
 
 
-
-% ============================================================
-
-end
