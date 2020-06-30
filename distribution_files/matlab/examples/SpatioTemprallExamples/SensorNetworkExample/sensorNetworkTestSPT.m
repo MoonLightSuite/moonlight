707d6ce07937b045671f305b31e7279cfbad5d4c
lalica <laura.nenzi@gmail.com>
2020-06-30 09:22:33 +0200

    testE

--- distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/sensorNetworkTestSPT.m
+++ distribution_files/matlab/examples/SpatioTemprallExamples/SensorNetworkExample/sensorNetworkTestSPT.m
@@ -2,15 +2,15 @@
 clear
 close all
 %% generation of the data (spatial model, time, values)
-numSteps        = 5;   % number of frames
-num_nodes       = 10;    % number of nodes
+numSteps        = 1;   % number of frames
+num_nodes       = 5;    % number of nodes
 framePlot = false; % to enable or disable the plot of the graph
 % see the sensorModel function for the description of the output
 [spatialModelv,spatialModelc, time, values]= sensorModel(num_nodes,numSteps, framePlot);
-inputModel =spatialModelc;
+inputModel =spatialModelv;
 % plot of the last frame
 % numframe = 1;
-% plotGraph(spatialModelv, 1 , 'node');
+plotGraph(inputModel, 1 , 'node');
 
 %% monitor
 % loading of the script
@@ -21,5 +21,6 @@ moonlightScript = ScriptLoader.loadFromFile("sensorNetMonitorScript");
 
 %moonlightScript.setMinMaxDomain(); % for the quantitative semantics
 moonlightScript.setBooleanDomain();
-P5Monitor = moonlightScript.getMonitor("PT2");
-P5Monitor = P5Monitor.monitor(inputModel,time,values);
+Monitor = moonlightScript.getMonitor("PE");
+MonitorResult = Monitor.monitor(inputModel,time,values);
+plotResults(inputModel, MonitorResult , true);
