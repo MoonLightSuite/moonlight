969f0301154bf837b568c2d1f719b9de66a3e223
michele-loreti <michele.loreti@unicam.it>
2020-12-29 16:44:53 +0100

    Renamed directory with examples.

---
+++ examples/spatio-temporal/epidemic/src/main/java/eu/quanticol/moonlight/examples/epidemic/EpidemicMain.java
@@ -0,0 +1,14 @@
+package eu.quanticol.moonlight.examples.epidemic;
+
+import eu.quanticol.moonlight.signal.Record;
+import eu.quanticol.moonlight.signal.SpatialModel;
+
+public class EpidemicMain {
+
+    public static void main(String[] argv) {
+        SpatialModel<Record> model = null;
+
+        System.out.println("Hello World!");
+    }
+
+}
