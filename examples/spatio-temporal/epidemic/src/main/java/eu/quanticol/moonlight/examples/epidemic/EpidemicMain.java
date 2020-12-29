e20920f0ca03fad450af8178feb45976450b6933
lalica <laura.nenzi@gmail.com>
2020-12-29 17:21:19 +0100

    epidemicExample

--- examples/spatio-temporal/epidemic/src/main/java/eu/quanticol/moonlight/examples/epidemic/EpidemicMain.java
+++ examples/spatio-temporal/epidemic/src/main/java/eu/quanticol/moonlight/examples/epidemic/EpidemicMain.java
@@ -1,14 +1,35 @@
 package eu.quanticol.moonlight.examples.epidemic;
 
-import eu.quanticol.moonlight.signal.Record;
-import eu.quanticol.moonlight.signal.SpatialModel;
+import eu.quanticol.moonlight.io.CsvLocationServiceReader;
+import eu.quanticol.moonlight.io.CsvSpatialTemporalSignalReader;
+import eu.quanticol.moonlight.io.IllegalFileFormatException;
+import eu.quanticol.moonlight.signal.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class EpidemicMain {
 
-    public static void main(String[] argv) {
-        SpatialModel<Record> model = null;
+    public static void main(String[] argv)  throws IllegalFileFormatException, URISyntaxException, IOException {
+        ClassLoader classLoader = EpidemicMain.class.getClassLoader();
 
-        System.out.println("Hello World!");
+        // space model
+        URL resourceL = classLoader.getResource("epidemic_simulation_network_0.txt");
+        File fileL = new File(resourceL.toURI());
+        CsvLocationServiceReader readerL =  new CsvLocationServiceReader();
+        RecordHandler rhL = new RecordHandler(DataHandler.REAL);
+        LocationService<Record> ls = readerL.read(rhL, fileL);
+
+        // trajectory
+        URL resourceT = classLoader.getResource("epidemic_simulation_trajectory_0.txt");
+        File fileT = new File(resourceT.toURI());
+        RecordHandler rhT = new RecordHandler(DataHandler.REAL);
+        CsvSpatialTemporalSignalReader readerT = new CsvSpatialTemporalSignalReader();
+        SpatialTemporalSignal<Record> s = readerT.load(rhT, fileT);
+
+
     }
 
 }
