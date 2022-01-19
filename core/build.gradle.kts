c3466b80ee62befdc0a50aa156c99de0ae37f152
Ennio Visconti <ennio.visconti@gmail.com>
2022-01-19 01:13:42 +0100

    build fix as from mater

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.8.9")
-    implementation("org.jgrapht:jgrapht-core:1.5.1")
+    implementation("org.jgrapht:jgrapht-core:1.4.0")
     implementation("org.jetbrains:annotations:23.0.0")
     implementation("com.github.sh0nk:matplotlib4j:0.5.0")
     implementation("org.apache.logging.log4j:log4j-core:2.17.1")
