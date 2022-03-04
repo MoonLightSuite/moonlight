7053b5a1c3fa08776d6b1fb37d3cd67f383ff8e0
Ennio Visconti <ennio.visconti@gmail.com>
2022-03-04 12:53:28 +0100

    added test class for Manhattan distance

---
+++ engine/src/test/java/eu/quanticol/moonlight/space/ManhattanDistanceStructureTest.java
@@ -0,0 +1,12 @@
+package eu.quanticol.moonlight.space;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManhattanDistanceStructureTest {
+    @Test
+    void simpleDistance() {
+
+    }
+}
