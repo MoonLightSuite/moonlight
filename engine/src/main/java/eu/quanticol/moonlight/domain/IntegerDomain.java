2c3ec20746628c365c4b57732138da6429cadf37
Ennio Visconti <ennio.visconti@gmail.com>
2022-03-04 16:28:30 +0100

    added missing test and integer distance

---
+++ engine/src/main/java/eu/quanticol/moonlight/domain/IntegerDomain.java
@@ -0,0 +1,38 @@
+package eu.quanticol.moonlight.domain;
+
+import eu.quanticol.moonlight.core.space.DistanceDomain;
+
+import java.util.Objects;
+
+public class IntegerDomain implements DistanceDomain<Integer> {
+    @Override
+    public Integer zero() {
+        return 0;
+    }
+
+    @Override
+    public Integer infinity() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public Integer sum(Integer x, Integer y) {
+        return x + y;
+    }
+
+    @Override
+    public boolean less(Integer x, Integer y) {
+        return x < y;
+    }
+
+    @Override
+    public boolean equalTo(Integer x, Integer y) {
+        return Objects.equals(x, y);
+    }
+
+    @Override
+    public boolean lessOrEqual(Integer x, Integer y) {
+        return x <= y;
+    }
+}
+
--- engine/src/test/java/eu/quanticol/moonlight/space/ManhattanDistanceStructureTest.java
+++ engine/src/test/java/eu/quanticol/moonlight/space/ManhattanDistanceStructureTest.java
@@ -1,12 +1,29 @@
 package eu.quanticol.moonlight.space;
 
+import eu.quanticol.moonlight.core.space.DistanceDomain;
+import eu.quanticol.moonlight.core.space.DistanceStructure;
+import eu.quanticol.moonlight.domain.IntegerDomain;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManhattanDistanceStructureTest {
     @Test
     void simpleDistance() {
+        DistanceDomain<Integer> domain = new IntegerDomain();
+        RegularGridModel<Integer> model =
+                new RegularGridModel<>(3, 3, 1);
+        DistanceStructure<Integer, Integer> dist =
+                new ManhattanDistanceStructure<>(defaultDistance(), domain,
+                                       0, 10, model);
 
+        assertEquals(2, dist.getDistance(0, 2));
+        assertEquals(1, dist.getDistance(0, 3));
+    }
+
+    private Function<Integer, Integer> defaultDistance() {
+        return x -> 1;
     }
 }
