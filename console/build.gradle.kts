a8fbcd34b1a3205a4566cae6c631da5d4da025e2
Ennio Visconti <ennio.visconti@gmail.com>
2023-03-13 11:19:00 +0100

    added python module scaffolding

--- console/build.gradle.kts
+++ console/build.gradle.kts
@@ -36,84 +36,63 @@ tasks.register("distribute-python") {
     dependsOn(project(":shared").tasks["build"])
     copy {
         from("$buildDir/libs/")
-        into(rootProject.file("$rootDir/../release/python"))
+        into(rootProject.file("$rootDir/release/python"))
     }
     copy {
         from("$buildDir/resources/main/python")
-        into(rootProject.file("$rootDir/../release/python"))
+        into(rootProject.file("$rootDir/release/python"))
     }
 }
 
 fun copyAuxiliaryFiles(source: String, destination: String) {
     copy {
         from("$buildDir/resources/main/$source")
-        into(rootProject.file("$rootDir/../release/$destination"))
+        into(rootProject.file("$rootDir/release/$destination"))
     }
 }
 
-
+fun copyFromTo(source: String, destination: String) =
+        copy {
+            println("Copying: ${file(source).listFiles()?.map{ it.name }} to $destination")
+            from(file(source))
+            into(rootProject.file(destination))
+        }
 
 tasks.register("distribution") {
-    logger.info("exec distribution Task!")
+    println("Executing :console:distribution.")
     dependsOn("jar")
     dependsOn("installDist")
 
     doLast {
-        copy {
-            from("$buildDir/libs/")
-            into(rootProject.file("$rootDir/../distribution_files/java/lib/"))
-        }
-
-        copy {
-            from("$buildDir/libs/")
-            into(rootProject.file("$rootDir/../distribution_files/matlab/moonlight/jar/"))
-        }
-
-        copy {
-            from("$buildDir/libs/")
-            into(rootProject.file("$rootDir/../distribution_files/python/jar/"))
-        }
-
-        copy {
-            from("$buildDir/install/mlconsole/")
-            into(rootProject.file("$rootDir/../distribution_files/console/"))
-        }
-
-        val files = file("$rootDir/../distribution_files/java/lib").listFiles()?.map{ it.name }
-        logger.info("Folders: ${files.toString()}")
+        copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/java/lib/")
+        copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/matlab/moonlight/jar/")
+        copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/python/jar/")
+        copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/console/")
     }
 }
-
-//  home/runner/work/MoonLight/MoonLight/ api/../distribution/java/lib
-// /home/runner/work/MoonLight/MoonLight/ distribution/java/lib/moonlight.jar
 
 tasks.register("release") {
     println("Executing :console:release.")
     dependsOn("distribution")
 
     doLast {
-        copy {
-            from(rootProject.file("$rootDir/../distribution_files/"))
-            into(rootProject.file("$rootDir/../distribution/"))
-            logger.info("jar saved in $rootDir/../distribution/java/lib")
-            val files = file("$rootDir/../distribution/java/lib").listFiles()?.map { it.name }
-            logger.info("Folders: ${files.toString()}")
-        }
+        val source = rootProject.file("$rootDir/distribution_files/").path
+        copyFromTo(source, "$rootDir/distribution/")
     }
 }
 
 tasks.named<Delete>("clean") {
     doFirst {
-        delete("$rootDir/../distribution/")
-        logger.info("jars in $rootDir/../distribution/ deleted")
-        delete("$rootDir/../distribution_files/java/lib/moonlight.jar")
-        logger.info("jars in $rootDir/../distribution_files/java/ deleted")
-        delete("$rootDir/../distribution_files/matlab/moonlight/jar/")
-        logger.info("jars in $rootDir/../distribution_files/matlab/ deleted")
-        delete("$rootDir/../distribution_files/python/jar/")
-        logger.info("jars in $rootDir/../distribution_files/python/ deleted")
-        delete("$rootDir/../distribution_files/console/")
-        logger.info("jars in $rootDir/../distribution_files/console/ deleted")
+        delete("$rootDir/distribution/")
+        println("jars in $rootDir/distribution/ deleted")
+        delete("$rootDir/distribution_files/java/lib/moonlight.jar")
+        println("jars in $rootDir/distribution_files/java/ deleted")
+        delete("$rootDir/distribution_files/matlab/moonlight/jar/")
+        println("jars in $rootDir/distribution_files/matlab/ deleted")
+        delete("$rootDir/distribution_files/python/jar/")
+        println("jars in $rootDir/distribution_files/python/ deleted")
+        delete("$rootDir/distribution_files/console/")
+        println("jars in $rootDir/distribution_files/console/ deleted")
     }
 }
 
