935cd05a8488ea8ef4ee7a9484a81d830187c3a6
Ennio Visconti <ennio.visconti@gmail.com>
2023-06-07 15:48:06 +0200

    updated python workflow

--- console/build.gradle.kts
+++ console/build.gradle.kts
@@ -14,36 +14,6 @@ application {
     applicationName = "mlconsole"
 }
 
-//tasks.jar {
-//    archiveFileName.set("moonlight.jar")
-//    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-//    manifest {
-//        attributes(mapOf("Main-Class" to application.mainClass))
-//    }
-//    doFirst {
-//        from({
-//            configurations.compileClasspath.get().map {
-//                if (it.isDirectory) it else zipTree(it)
-//            }
-//        })
-//    }
-//    exclude ("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
-//}
-
-tasks.register("distribute-python") {
-    dependsOn("build")
-
-    dependsOn(project(":shared").tasks["build"])
-    copy {
-        from("$buildDir/libs/")
-        into(rootProject.file("$rootDir/release/python"))
-    }
-    copy {
-        from("$buildDir/resources/main/python")
-        into(rootProject.file("$rootDir/release/python"))
-    }
-}
-
 fun copyAuxiliaryFiles(source: String, destination: String) {
     copy {
         from("$buildDir/resources/main/$source")
@@ -64,9 +34,7 @@ tasks.register("distribution") {
     dependsOn("installDist")
 
     doLast {
-        copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/java/lib/")
         copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/matlab/moonlight/jar/")
-        copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/python/jar/")
         copyFromTo("$buildDir/libs/", "$rootDir/distribution_files/console/")
     }
 }
@@ -85,12 +53,8 @@ tasks.named<Delete>("clean") {
     doFirst {
         delete("$rootDir/distribution/")
         println("jars in $rootDir/distribution/ deleted")
-        delete("$rootDir/distribution_files/java/lib/moonlight.jar")
-        println("jars in $rootDir/distribution_files/java/ deleted")
         delete("$rootDir/distribution_files/matlab/moonlight/jar/")
         println("jars in $rootDir/distribution_files/matlab/ deleted")
-        delete("$rootDir/distribution_files/python/jar/")
-        println("jars in $rootDir/distribution_files/python/ deleted")
         delete("$rootDir/distribution_files/console/")
         println("jars in $rootDir/distribution_files/console/ deleted")
     }
